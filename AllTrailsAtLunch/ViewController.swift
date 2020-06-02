e9159c88ccb69ec4afcdbdce889ebde116d55990
evelynccordner <ecordner@gmail.com>
2020-06-02 10:45:05 -0700

    Initial Commit

---
+++ AllTrailsAtLunch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AllTrailsAtLunch
+//
+//  Created by Evelyn C Cordner on 6/2/20.
+//  Copyright © 2020 EvelynCordner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
